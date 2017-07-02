c6536fd2d4fddbe9978b4cc3cbf54fa14a0cf811
kuuuurija <karthikeyanvj@gmail.com>
2017-07-02 14:39:12 +0300

    update

--- src/js/background/sites/err.ee.js
+++ src/js/background/sites/err.ee.js
@@ -11,18 +11,35 @@
             this.liveStreams = {
                 "etv": "http://etvstream.err.ee/live/smil:etv/playlist.m3u8",
                 "etv2": "http://etv2stream.err.ee/live/smil:etv2/playlist.m3u8",
-                "etv+": "http://striimid.err.ee/live/smil:etvpluss/playlist.m3u8"
+                "etvpluss": "http://striimid.err.ee/live/smil:etvpluss/playlist.m3u8"
             };
 
+            // alias
+            this.liveStreams["etv+"] = this.liveStreams["etvpluss"];
+
             contextMenu.addSite("err.ee", this, ["*://*.err.ee/v/*/*-*-*-*-*/*"]);
-            browserAction.addSite("err.ee", this, [".*err.ee/v/.*/.*-.*-.*-.*-.*/.*"]);
+            browserAction.addSite("err.ee", this, [
+                ".*err.ee/v/.*/.*-.*-.*-.*-.*/.*",
+                ".*otse.err.ee/k/.*"
+            ]);
         }
 
         _getVideoIdFromUrl(url)
         {
-            url = url.slice(0, -1);
             let parts = url.split("/");
-            return parts[parts.length - 2];
+            let lastChar = parts[parts.length - 1];
+            if(!lastChar) {
+                parts.pop();
+            }
+            let hasVOD = url.indexOf("/v/") >= 0;
+            let starIndex = hasVOD ? parts.length - 2 : parts.length - 1;
+            for (let i = starIndex; i >= 0; i--) {
+                let text = parts[i];
+                if(text) {
+                    return text;
+                }
+            }
+            return "";
         }
 
         getFileFromUrl(url)
@@ -30,13 +47,24 @@
             return new Promise((resolve, reject) => {
 
                 let videoId = this._getVideoIdFromUrl(url);
+                console.log("videoId " + videoId);
+
+                const isLive = url.indexOf("otse.err.ee") >= 0;
+                if(isLive) {
+                    console.log("live video");
+                    let mediaUrl = this.liveStreams[videoId];
+                    if(mediaUrl) {
+                        resolve(mediaUrl);
+                        return;
+                    }
+                }
+
                 const apiUrl = sprintf(this.contentApiUrl, videoId);
 
                 let urlRequest = new URLRequest(apiUrl);
                 urlRequest.send().then(response => {
 
                     const responseData = JSON.parse(response);
-
                     let mediaSources = responseData.MediaSources;
                     const len = mediaSources.length;
 
