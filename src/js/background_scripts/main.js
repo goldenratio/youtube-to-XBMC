b7feba9f43b425214da8cab00377841fa1a4bd4d
kuuuurija <karthikeyanvj@gmail.com>
2017-07-02 17:30:43 +0300

    update

--- src/js/background_scripts/main.js
+++ src/js/background_scripts/main.js
@@ -524,59 +524,6 @@ class ContextMenu
     }
 }
 
-class URLRequest
-{
-    constructor(url)
-    {
-        this._url = url;
-        this._contentType = "application/json";
-        this._method = "GET";
-    }
-
-    set contentType(value)
-    {
-        this._contentType = value;
-    }
-
-    set method(value)
-    {
-        this._method = value;
-    }
-
-    send(data = "")
-    {
-        //console.log("send url request " + this._url);
-        return new Promise((reslove, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open(this._method, this._url, true);
-            xhr.setRequestHeader("Content-type", this._contentType);
-
-            xhr.onreadystatechange = function() {
-                //console.log("this.readyState, " + this.readyState + ", " + this.status);
-            };
-
-            xhr.onerror = function() {
-                reject();
-            };
-
-            xhr.onload = function () {
-                const isSuccess = (this.status == 200);
-                if(isSuccess)
-                {
-                    reslove(this.responseText);
-                }
-                else
-                {
-                    reject();
-                }
-            };
-
-            xhr.send(data);
-        });
-    }
-}
-
 class AbstractSite
 {
     constructor()
---
+++ src/js/shared.js
@@ -0,0 +1,49 @@
+class URLRequest
+{
+    constructor(url)
+    {
+        this._url = url;
+        this._contentType = "application/json";
+        this._method = "GET";
+    }
+
+    set contentType(value)
+    {
+        this._contentType = value;
+    }
+
+    set method(value)
+    {
+        this._method = value;
+    }
+
+    send(data = "")
+    {
+        return new Promise((reslove, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open(this._method, this._url, true);
+            xhr.setRequestHeader("Content-type", this._contentType);
+
+            xhr.onreadystatechange = function() {};
+
+            xhr.onerror = function() {
+                reject();
+            };
+
+            xhr.onload = function () {
+                const isSuccess = (this.status == 200);
+                if(isSuccess)
+                {
+                    reslove(this.responseText);
+                }
+                else
+                {
+                    reject();
+                }
+            };
+
+            xhr.send(data);
+        });
+    }
+}
