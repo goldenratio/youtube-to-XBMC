ae15ded728c040f2226dbdf195d662547ab9b59a
kuuuurija <karthikeyanvj@gmail.com>
2017-07-25 02:56:32 +0300

    update

--- src/js/content_scripts/sites/legacy_youtube.js
+++ src/js/content_scripts/sites/legacy_youtube.js
@@ -455,7 +455,7 @@
 
     chrome.storage.local.get(function(item)
     {
-        if(!item.showInPage || (item.showInPage && item.showInPage == true))
+        if(typeof item.showInPage === "undefined" || (item.showInPage && item.showInPage == true))
         {
             new ContentInjector();
         }
