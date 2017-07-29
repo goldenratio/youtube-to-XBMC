419993de5d71d9de7b2eab5055a841abc2b083b6
kuuuurija <karthikeyanvj@gmail.com>
2017-07-29 10:31:51 +0300

    initial support for crunchyroll

---
+++ src/js/background_scripts/sites/crunchyroll.com.js
@@ -0,0 +1,31 @@
+;(function(player)
+{
+
+    class CrunchyRoll extends AbstractSite
+    {
+        constructor(player)
+        {
+            super(player);
+            console.log("CrunchyRoll");
+
+            this.pluginURL = "plugin://plugin.video.crunchyroll-takeout/?mode=videoplay&url=%s";
+
+            browserAction.addSite("crunchyroll.com", this, [
+                ".*crunchyroll.com/.*-\\d+"
+            ]);
+        }
+
+        getFileFromUrl(url)
+        {
+            return new Promise((resolve, reject) => {
+
+                let mediaUrl = sprintf(this.pluginURL, url);
+                console.log("mediaUrl " + mediaUrl);
+                resolve(mediaUrl);
+            });
+        }
+
+    }
+
+    new CrunchyRoll(player)
+})(player);
