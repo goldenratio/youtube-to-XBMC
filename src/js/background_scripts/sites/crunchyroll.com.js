30694be6e106ea8ddda95c71159738dcf2cf6133
kuuuurija <karthikeyanvj@gmail.com>
2017-07-29 14:32:06 +0300

    updated context menu pattern

--- src/js/background_scripts/sites/crunchyroll.com.js
+++ src/js/background_scripts/sites/crunchyroll.com.js
@@ -10,7 +10,7 @@
 
             this.pluginURL = "plugin://plugin.video.crunchyroll-takeout/?mode=videoplay&id=%s&url=%s&name=%s&season=0&icon=None&duration=0";
 
-            contextMenu.addSite("crunchyroll.com", this, ["*://www.crunchyroll.com/*"]);
+            contextMenu.addSite("crunchyroll.com", this, ["*://www.crunchyroll.com/*-*"]);
             browserAction.addSite("crunchyroll.com", this, [
                 ".*crunchyroll.com/.*-\\d+"
             ]);
